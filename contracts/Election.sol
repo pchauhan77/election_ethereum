f3bdefc6216e97516c0d684e64a737ae1ab68399
PRADEEP KUMAR CHAUHAN <pradeep.chauhan67@gmail.com>
2018-09-11 15:19:17 +0530

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+contract Election {
+	// Store candidate
+	// Read candidate
+	string public candidate;
+	// constructor
+    function Election1 () public {
+    	candidate = "Candidate 1";
+
+    }
+}
